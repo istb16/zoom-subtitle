41365a93a9f476af1f3b958658b1ff2e0866eb85
Takayuki Hirasawa <istb16@gmail.com>
2022-08-24 00:13:33 +0900

    ztディレクトリ対応

--- src/App.svelte
+++ src/App.svelte
@@ -5,11 +5,11 @@
 
 <Router>
   <nav>
-    <Link to="./">ホーム</Link>
+    <Link to="/zt">ホーム</Link>
   </nav>
   <main>
     <h1>Zoom字幕</h1>
-    <Route path="" component={Home} />
+    <Route path="/zt" component={Home} />
   </main>
   <style lang="sass">
 
